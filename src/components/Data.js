7b0187debde414d10e2d82efd088e2167722047b
Anushtha <kumarsen.bash@gmail.com>
2023-11-12 15:34:00 +0530

    mobile phones section

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,47 +1,40 @@
 const Data = {
-  productItems: [
-    {
-      id: 1,
-      discount: 50,
-      cover: "./images/flash/flash-1.png",
-      name: "Shoes",
-      price: 100,
-    },
-    {
-      id: 2,
-      discount: 40,
-      cover: "./images/flash/flash-2.png",
-      name: "Watch",
-      price: 20,
-    },
-    {
-      id: 3,
-      discount: 40,
-      cover: "./images/flash/flash-3.png",
-      name: "Smart Mobile Black",
-      price: 200,
-    },
-    {
-      id: 4,
-      discount: 40,
-      cover: "./images/flash/flash-4.png",
-      name: "Smart Watch Black",
-      price: 50,
-    },
-    {
-      id: 5,
-      discount: 50,
-      cover: "./images/flash/flash-1.png",
-      name: "Shoes",
-      price: 100,
-    },
-    {
-      id: 6,
-      discount: 50,
-      cover: "./images/flash/flash-3.png",
-      name: "Shoes",
-      price: 100,
-    },
-  ],
-}
-export default Data
+	productItems: [
+		{
+			id: 1,
+			discount: 50,
+			cover: "/images/product/iphone-13.jpg",
+			name: "Apple iPhone 13 Pro",
+			price: 999,
+		},
+		{
+			id: 2,
+			discount: 40,
+			cover: "/images/product/s22.jpg",
+			name: "Samsung Galaxy S22 Ultra",
+			price: 1199,
+		},
+		{
+			id: 3,
+			discount: 40,
+			cover: "/images/product/6pro.jpg",
+			name: "Google Pixel 6 Pro",
+			price: 899,
+		},
+		{
+			id: 4,
+			discount: 40,
+			cover: "/images/product/one-plus.jpeg",
+			name: "OnePlus 10 Pro",
+			price: 799,
+		},
+		{
+			id: 5,
+			discount: 50,
+			cover: "/images/product/xiaomi-12.png",
+			name: "Xiaomi 12 Pro",
+			price: 699,
+		},
+	],
+};
+export default Data;
